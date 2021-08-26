25ac8bcefc28b2798af3fcc52933aae67abb720c
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-08-26 08:52:41 -0300

    Consuming github repos with useEffect and fetch

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 const repository = {
   name: "Repository 1",
@@ -9,6 +10,16 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/ajvideira/repos")
+      .then((response) => response.json())
+      .then((json) => setRepositories(json));
+  }, []);
+
+  console.log("Repositories: ", repositories);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
